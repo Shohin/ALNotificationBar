a9a68364bca116ce35e795e1d75d064fd1214ad1
Shohin Tagaev <shohin@Shohins-Mac.local>
2019-01-16 03:33:20 -0800

    Initial Commit

---
+++ ALNotificationBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ALNotificationBar
+//
+//  Created by Shohin Tagaev on 1/16/19.
+//  Copyright © 2019 shohin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
